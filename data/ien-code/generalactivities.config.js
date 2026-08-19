8a68e4645c46156b9f404872aa561c8d3f4b0f02
ien-code-probe <actions@users.noreply.github.com>
2026-08-19 05:13:30 +0000

    chore: capture IEN activity controllers

---
+++ data/ien-code/generalactivities.config.js
@@ -0,0 +1,37 @@
+﻿(function () {
+    angular.module('ngt4edu.portal.generalactivities')
+        .config(['$stateProvider', '$urlMatcherFactoryProvider', routs]);
+    function routs($stateProvider, $urlMatcherFactoryProvider) {
+        $urlMatcherFactoryProvider.strictMode(false);
+        $stateProvider
+            .state('studentactivitiesmainpage', {
+                url: '/studentactivities',
+                views: {
+                    "main": {
+                        templateUrl: 'app/portal/generalactivities/studentactivities.tpl.html?v=47',
+                        controller: 'studentactivitiesCtrl'
+                    }
+                }
+            })
+            .state('generalactivitiesmainpage', {
+                url: '/generalactivities/:categoryId',
+                views: {
+                    "main": {
+                        templateUrl: 'app/portal/generalactivities/generalactivitiespage.tpl.html?v=47',
+                        controller: 'generalactivitiespageCtrl'
+                    }
+                }
+            })
+            .state('generalactivitiespackages', {
+                url: '/generalactivitiespackages/:categoryId',
+                views: {
+                    "main": {
+                        templateUrl: 'app/portal/generalactivities/generalactivitiespackages.tpl.html?v=47',
+                        controller: 'generalactivitiespackagesCtrl'
+                    }
+                }
+            })
+            ;
+    };
+})();
+
---
+++ data/ien-code/generalactivities.module.js
@@ -0,0 +1,4 @@
+﻿'use strict';
+(function (angular) {
+    angular.module('ngt4edu.portal.generalactivities', ['ui.router']);
+})(angular);
